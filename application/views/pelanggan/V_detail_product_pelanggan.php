c175a3d9e9fcc65fc7e69fd104299150dbc8c629
Amarizky <amarizky02@gmail.com>
2021-09-07 14:53:48 +0200

    commit

--- application/views/pelanggan/V_detail_product_pelanggan.php
+++ application/views/pelanggan/V_detail_product_pelanggan.php
@@ -107,19 +107,94 @@
                                 <div class="grid-item">
                                     <b>Personalisasi</b>
                                     <br><br>
-                                    <input type="radio" id="persona1" placeholder="Personalisasi" name="persona1" value="1">
-                                    <label for="persona1">Blanko</label><br>
-                                    <input type="radio" id="persona2" placeholder="Personalisasi" name="persona2" value="2">
-                                    <label for="persona2">Nomerator</label><br>
-                                    <input type="radio" id="persona3" placeholder="Personalisasi" name="persona3" value="3">
-                                    <label for="persona3">Barcode</label><br>
-                                    <input type="radio" id="persona4" placeholder="Personalisasi" name="persona4" value="4">
-                                    <label for="persona4">Data</label><br>
-                                    <input type="radio" id="persona5" placeholder="Personalisasi" name="persona5" value="5">
-                                    <label for="persona5">Data + Foto</label>
+                                    <div class="form-group">
+                                        <input type="radio" id="persona1" placeholder="Personalisasi" name="personalisasi" value="1">
+                                        <label for="persona1">Blanko</label><br>
+                                        <input type="radio" id="persona2" placeholder="Personalisasi" name="personalisasi" value="2">
+                                        <label for="persona2">Nomerator</label><br>
+                                        <input type="radio" id="persona3" placeholder="Personalisasi" name="personalisasi" value="3">
+                                        <label for="persona3">Barcode</label><br>
+                                        <input type="radio" id="persona4" placeholder="Personalisasi" name="personalisasi" value="4">
+                                        <label for="persona4">Data</label><br>
+                                        <input type="radio" id="persona5" placeholder="Personalisasi" name="personalisasi" value="5">
+                                        <label for="persona5">Data + Foto</label>
+                                    </div>
                                 </div>
                                 <div class="grid-item">
-                                    <b> </b>
+                                    <b>Coating</b>
+                                    <br><br>
+                                    <input type="radio" id="coating1" placeholder="Coating" name="coating" value="1">
+                                    <label for="coating1">Glossy</label><br>
+                                    <input type="radio" id="coating2" placeholder="Coating" name="coating" value="2">
+                                    <label for="coating2">Doff</label><br>
+                                    <input type="radio" id="coating3" placeholder="Coating" name="coating" value="3">
+                                    <label for="coating3">Glossy + Doff</label>
+                                </div>
+                                <div class="grid-item">
+                                    <b>Finishing</b>
+                                    <br><br>
+                                    <input type="checkbox" id="finish1" placeholder="Finishing" name="finishing" value="1">
+                                    <label for="finish1">Tidak ada</label><br>
+                                    <input type="checkbox" id="finish2" placeholder="Finishing" name="finishing" value="2">
+                                    <label for="finish2">Urutkan</label><br>
+                                    <input type="checkbox" id="finish3" placeholder="Finishing" name="finishing" value="3">
+                                    <label for="finish3">Label Gosok</label><br>
+                                    <input type="checkbox" id="finish4" placeholder="Finishing" name="finishing" value="4">
+                                    <label for="finish4">Plong Oval</label><br>
+                                    <input type="checkbox" id="finish5" placeholder="Finishing" name="finishing" value="5">
+                                    <label for="finish5">Plong Bulat</label><br>
+                                    <input type="checkbox" id="finish6" placeholder="Finishing" name="finishing" value="6">
+                                    <label for="finish6">Copy Data RFID</label><br>
+                                    <input type="checkbox" id="finish7" placeholder="Finishing" name="finishing" value="7">
+                                    <label for="finish7">Emboss Silver</label><br>
+                                    <input type="checkbox" id="finish8" placeholder="Finishing" name="finishing" value="8">
+                                    <label for="finish8">Emboss Gold</label><br>
+                                    <input type="checkbox" id="finish9" placeholder="Finishing" name="finishing" value="9">
+                                    <label for="finish9">Panel</label><br>
+                                    <input type="checkbox" id="finish10" placeholder="Finishing" name="finishing" value="10">
+                                    <label for="finish10">Hot Print</label><br>
+                                    <input type="checkbox" id="finish11" placeholder="Finishing" name="finishing" value="11">
+                                    <label for="finish11">Swipe</label><br>
+                                </div>
+                                <div class="grid-item">
+                                    <b>Function</b>
+                                    <br><br>
+                                    <input type="radio" id="function1" placeholder="Function" name="function" value="1">
+                                    <label for="function1">Print Thermal</label><br>
+                                    <input type="radio" id="function2" placeholder="Function" name="function" value="2">
+                                    <label for="function2">Scan Barcode</label><br>
+                                    <input type="radio" id="function3" placeholder="Function" name="function" value="3">
+                                    <label for="function3">Swipe Magnetic</label><br>
+                                    <input type="radio" id="function4" placeholder="Function" name="function" value="4">
+                                    <label for="function4">Tap RFID</label>
+                                </div>
+                                <div class="grid-item">
+                                    <b>Packaging</b>
+                                    <br><br>
+                                    <input type="radio" id="packaging1" placeholder="Packaging" name="packaging" value="1">
+                                    <label for="packaging1">Plastik 1 on 1</label><br>
+                                    <input type="radio" id="packaging2" placeholder="Packaging" name="packaging" value="2">
+                                    <label for="packaging2">Plastik Terpisah</label><br>
+                                    <input type="radio" id="packaging3" placeholder="Packaging" name="packaging" value="3">
+                                    <label for="packaging3">Box Kartu Nama</label><br>
+                                    <input type="radio" id="packaging4" placeholder="Packaging" name="packaging" value="4">
+                                    <label for="packaging4">Box Putih</label><br>
+                                    <input type="radio" id="packaging5" placeholder="Packaging" name="packaging" value="5">
+                                    <label for="packaging5">Small UCARD</label><br>
+                                    <input type="radio" id="packaging6" placeholder="Packaging" name="packaging" value="6">
+                                    <label for="packaging6">Small Maxi UCARD</label><br>
+                                    <input type="radio" id="packaging7" placeholder="Packaging" name="packaging" value="7">
+                                    <label for="packaging7">Large UCARD</label><br>
+                                    <input type="radio" id="packaging8" placeholder="Packaging" name="packaging" value="8">
+                                    <label for="packaging8">Large Maxi UCARD</label>
+                                </div>
+                                <div class="grid-item">
+                                    <b>Ambil/Kirim</b>
+                                    <br><br>
+                                    <input type="radio" id="kirim" placeholder="Ambil/Kirim" name="status" value="1">
+                                    <label for="kirim">Kirim Product</label><br>
+                                    <input type="radio" id="ambil" placeholder="Ambil/Kirim" name="Status" value="2">
+                                    <label for="ambil">Ambil Sendiri</label>
                                 </div>
                             </div>
                             <div class="form-group">
